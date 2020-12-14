72627fed72a503044876cacbb89efe5ac430a2d9
Aamir Alam <aamiralam1991@gmail.com>
2020-12-14 19:21:12 +0530

    Fix on edit input field UI

--- app.js
+++ app.js
@@ -1,59 +1,103 @@
 // selectors
 
-const todoInput = document.querySelector('.todo-input');
-const todoAddButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const groceryInput = document.querySelector('.grocery-input');
+const groceryAddButton = document.querySelector('.add-grocery-button');
+const groceryList = document.querySelector('.grocery-list');
 
 // event listners
 
-todoAddButton.addEventListener('click', addGrocry )
+groceryAddButton.addEventListener('click', addGrocry )
 
-todoList.addEventListener('click', deleteCheck);
+groceryList.addEventListener('click', deleteCheck);
 
 // functions
 
 function addGrocry(event){
 
     event.preventDefault();
+    
+    if (!groceryInput.value) {
+        return
+    }
 
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
+    const groceryDiv = document.createElement('div');
+    groceryDiv.classList.add('grocery');
 
-    const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item')
+    const newgrocery = document.createElement('li');
+    newgrocery.innerText = groceryInput.value;
+    newgrocery.classList.add('grocery-item')
 
-    todoDiv.appendChild(newTodo);
+    groceryDiv.appendChild(newgrocery);
 
     const editButton = document.createElement('button');
     editButton.innerHTML = '<i class="fas fa-edit" ></i>';
     editButton.classList.add('edit-button')
-    todoDiv.appendChild(editButton)
+    groceryDiv.appendChild(editButton)
 
 
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash" ></i>';
     deleteButton.classList.add('delete-button')
-    todoDiv.appendChild(deleteButton)
+    groceryDiv.appendChild(deleteButton)
 
-    todoList.appendChild(todoDiv)
+    groceryList.appendChild(groceryDiv)
     console.log('adding new item')
 
-    //clear todo input value
-    todoInput.value = ""
+    //clear grocery input value
+    groceryInput.value = ""
 
 }
 
 function deleteCheck(e) {
 
     const item = e.target;
+    console.log(e)
 
     if (item.classList[0] == "delete-button") {
-        const todo = item.parentElement;
-        todo.classList.add("fall")
-        todo.remove()
+        const grocery = item.parentElement;
+        grocery.classList.add("fall")
+        grocery.remove()
     }else if (item.classList[0] == "edit-button") {
+        const groceryDiv = document.createElement('div');
+        groceryDiv.classList.add('grocery');
+    
+        const newGrocery = document.createElement('input');
+        newGrocery.value =  item.value;
+        newGrocery.classList.add('edit-input')
+    
+        groceryDiv.appendChild(newGrocery);
+    
+    
+        const saveBtn = document.createElement('button');
+        saveBtn.innerHTML = '<i class="fas fa-check" ></i>';
+        saveBtn.classList.add('delete-button')
+        groceryDiv.appendChild(saveBtn)
         
+        item.parentElement.replaceWith(groceryDiv)
+    }else if(item.classList[0] == "save-button"){
+        const groceryDiv = document.createElement('div');
+        groceryDiv.classList.add('grocery');
+    
+        const newGrocery = document.createElement('li');
+        newGrocery.innerText = groceryInput.value;
+        newGrocery.classList.add('grocery-item')
+    
+        groceryDiv.appendChild(newGrocery);
+    
+        const editButton = document.createElement('button');
+        editButton.innerHTML = '<i class="fas fa-edit" ></i>';
+        editButton.classList.add('edit-button')
+        groceryDiv.appendChild(editButton)
+    
+    
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash" ></i>';
+        deleteButton.classList.add('save-button')
+        groceryDiv.appendChild(deleteButton)
+        
+        item.parentElement.replaceWith(groceryDiv)
+
+        console.log('save edited item')
     }
 
 }
