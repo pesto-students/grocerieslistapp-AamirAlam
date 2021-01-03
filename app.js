47bd0e4db25aea169ce0f22f5b07be051d951521
Aamir Alam <aamiralam1991@gmail.com>
2021-01-03 13:15:28 +0530

    Fix Edit events in grocery items

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // selectors
-
 const groceryInput = document.querySelector('.grocery-input');
+groceryInput.focus()
+
 const groceryAddButton = document.querySelector('.add-grocery-button');
 const groceryList = document.querySelector('.grocery-list');
 
+const editButton = document.querySelector('.edit-button')
+const deleteButton = document.querySelector('.delete-button')
+const saveButton = document.querySelector('.save-button')
+
+const groceryEditInput = document.querySelector('.edit-input');
 // event listners
 
 groceryAddButton.addEventListener('click', addGrocry )
-
-groceryList.addEventListener('click', deleteCheck);
 
 // functions
 
@@ -30,14 +34,14 @@ function addGrocry(event){
     groceryDiv.appendChild(newgrocery);
 
     const editButton = document.createElement('button');
-    editButton.innerHTML = '<i class="fas fa-edit" ></i>';
-    editButton.classList.add('edit-button')
+    editButton.innerHTML = '<i class="fas fa-edit edit-button" ></i>';
+    editButton.addEventListener('click', editItem)
     groceryDiv.appendChild(editButton)
 
 
     const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<i class="fas fa-trash" ></i>';
-    deleteButton.classList.add('delete-button')
+    deleteButton.innerHTML = '<i class="fas fa-trash delete-button" ></i>';
+    deleteButton.addEventListener('click', deleteItem)
     groceryDiv.appendChild(deleteButton)
 
     groceryList.appendChild(groceryDiv)
@@ -48,57 +52,60 @@ function addGrocry(event){
 
 }
 
-function deleteCheck(e) {
+function editItem(event) {
+    const item = event.target;
+    console.log(item)
+    const groceryDiv = document.createElement('div');
+    groceryDiv.classList.add('grocery');
 
-    const item = e.target;
-    console.log(e)
+    const newGrocery = document.createElement('input');
+    newGrocery.value =  item.parentElement.innerText;
+    newGrocery.classList.add('edit-input')
+    newGrocery.focus()
+    groceryDiv.appendChild(newGrocery);
 
-    if (item.classList[0] == "delete-button") {
-        const grocery = item.parentElement;
-        grocery.classList.add("fall")
-        grocery.remove()
-    }else if (item.classList[0] == "edit-button") {
-        const groceryDiv = document.createElement('div');
-        groceryDiv.classList.add('grocery');
-    
-        const newGrocery = document.createElement('input');
-        newGrocery.value =  item.value;
-        newGrocery.classList.add('edit-input')
-    
-        groceryDiv.appendChild(newGrocery);
-    
-    
-        const saveBtn = document.createElement('button');
-        saveBtn.innerHTML = '<i class="fas fa-check" ></i>';
-        saveBtn.classList.add('delete-button')
-        groceryDiv.appendChild(saveBtn)
-        
-        item.parentElement.replaceWith(groceryDiv)
-    }else if(item.classList[0] == "save-button"){
-        const groceryDiv = document.createElement('div');
-        groceryDiv.classList.add('grocery');
-    
-        const newGrocery = document.createElement('li');
-        newGrocery.innerText = groceryInput.value;
-        newGrocery.classList.add('grocery-item')
-    
-        groceryDiv.appendChild(newGrocery);
-    
+
+    const saveBtn = document.createElement('button');
+    saveBtn.innerHTML = '<i class="fas fa-check save-button" ></i>';
+    groceryDiv.appendChild(saveBtn)
+
+    saveBtn.addEventListener('click', function() {
+
+
+        const groceryLi = document.createElement('li');
+        groceryLi.innerText =newGrocery.value;
+        groceryLi.classList.add('grocery-item')
+
+        // groceryDiv.parentElement.removeChild(groceryDiv);
+
+        const EditedgroceryDiv = document.createElement('div');
+        EditedgroceryDiv.classList.add('grocery');
+
+        EditedgroceryDiv.appendChild(groceryLi);
+
         const editButton = document.createElement('button');
-        editButton.innerHTML = '<i class="fas fa-edit" ></i>';
-        editButton.classList.add('edit-button')
-        groceryDiv.appendChild(editButton)
+        editButton.innerHTML = '<i class="fas fa-edit edit-button" ></i>';
+        editButton.addEventListener('click', editItem)
+        EditedgroceryDiv.appendChild(editButton)
     
     
         const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = '<i class="fas fa-trash" ></i>';
-        deleteButton.classList.add('save-button')
-        groceryDiv.appendChild(deleteButton)
-        
-        item.parentElement.replaceWith(groceryDiv)
+        deleteButton.innerHTML = '<i class="fas fa-trash delete-button" ></i>';
+        deleteButton.addEventListener('click', deleteItem)
+        EditedgroceryDiv.appendChild(deleteButton)
 
-        console.log('save edited item')
-    }
+        groceryDiv.replaceWith(EditedgroceryDiv)
+    } )
+    
+    item.parentElement.replaceWith(groceryDiv)
+}
 
+function deleteItem(event){
+    console.log('delete item')
+    const item = event.target;
+    console.log(item.id)
+    const grocery = item.parentElement;
+    grocery.classList.add("fall")
+    grocery.remove()
 }
 
